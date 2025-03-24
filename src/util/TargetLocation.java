6f46062140ec0dccb6e73a2dd49f6ba4cad45e38
Shawn Lin <spig100.roc@gmail.com>
2025-03-24 21:59:54 +0800

    feat: Adding quaternionToRotationMatrix function
    issue #1

--- src/util/TargetLocation.java
+++ src/util/TargetLocation.java
@@ -1,7 +1,12 @@
 package util;
 
 public class TargetLocation {
-    public void getTargetGlobalPosition(){
-
+    public static double[][] quaternionToRotationMatrix(Quaternion quaternion) {
+        double w = quaternion.getW(), x = quaternion.getX(), y = quaternion.getY(), z = quaternion.getZ();
+        return new double[][] {
+                {1 - 2*y*y - 2*z*z, 2*x*y - 2*w*z,      2*x*z + 2*w*y       },
+                {2*x*y + 2*w*z,     1 - 2*x*x - 2*z*z,  2*y*z - 2*w*x       },
+                {2*x*z - 2*w*y,     2*y*z + 2*w*x,      1 - 2*x*x - 2*y*y   }
+        };
     }
 }
