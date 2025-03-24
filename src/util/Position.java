0598f563eb93b28074419bd1c5955c6fa3434fc1
Shawn Lin <spig100.roc@gmail.com>
2025-03-24 14:58:27 +0800

    feat: Adding Position class

---
+++ src/util/Position.java
@@ -0,0 +1,11 @@
+package util;
+
+public class Position {
+    private final double x, y, z;
+
+    public Position(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
