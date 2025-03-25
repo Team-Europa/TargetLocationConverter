2781099e6670dbc194bf08dd99232480c962e6bd
Shawn Lin <spig100.roc@gmail.com>
2025-03-25 10:08:17 +0800

    test: Adding IO for testing

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,42 @@ import util.Position;
 import util.Quaternion;
 import util.TargetLocation;
 
+import java.util.Scanner;
+
 public class Main {
     /*
      * This is a test environment for the TargetLocation.
      * In this test, the expected output would be: "Global Position: (4, 2, 5)"
      */
     public static void main(String[] args) {
-        Position localOrigin = new Position(3,2,5);
-        double angle = Math.toRadians(0);
-        Quaternion orientation = new Quaternion(
-                Math.cos(angle/2),
-                0,
-                Math.sin(angle/2),
-                0
-        );
-        Position localPosition = new Position(1,0,0);
+        Scanner scanner = new Scanner(System.in);
+
+        // Test data inputting
+        scanner.useDelimiter(",");
+        System.out.println("Enter the local position of the target (x, y, z): ");
+        String inputLine = scanner.nextLine();
+        String[] inputLn = inputLine.trim().split(",");
+        double localX = Double.parseDouble(inputLn[0]);
+        double localY = Double.parseDouble(inputLn[1]);
+        double localZ = Double.parseDouble(inputLn[2]);
+        Position localPosition = new Position(localX, localY, localZ);
+
+        System.out.println("Enter the robot global position (x, y, z): ");
+        String inputLine2 = scanner.nextLine();
+        String[] inputLn2 = inputLine2.trim().split(",");
+        double robotX = Double.parseDouble(inputLn2[0]);
+        double robotY = Double.parseDouble(inputLn2[1]);
+        double robotZ = Double.parseDouble(inputLn2[2]);
+        Position localOrigin = new Position(robotX, robotY, robotZ);
+
+        System.out.println("%nEnter the robot orientation (w, x, y, z): ");
+        String inputLine3 = scanner.nextLine();
+        String[] inputLn3 = inputLine3.trim().split(",");
+        double orientationW = Double.parseDouble(inputLn3[0]);
+        double orientationX = Double.parseDouble(inputLn3[1]);
+        double orientationY = Double.parseDouble(inputLn3[2]);
+        double orientationZ = Double.parseDouble(inputLn3[3]);
+        Quaternion orientation = new Quaternion(orientationW, orientationX, orientationY, orientationZ);
 
         Position globalPoint = TargetLocation.transformPosition(localPosition, localOrigin, orientation);
 
